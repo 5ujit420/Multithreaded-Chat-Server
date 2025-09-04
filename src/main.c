e7ca847fd569ba725949ba6397096bd047075de6
5ujit420 <sujittayade2040@gmail.com>
2025-09-05 01:49:50 +0530

    new version

--- src/main.c
+++ src/main.c
@@ -1,22 +1,10 @@
-#include "../include/logger.h"
-#include "../include/server.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-    exit(EXIT_FAILURE);
+    printf("Usage: %s <port>", argv[0]);
+    return 0;
   }
 
-  int port = atoi(argv[1]);
-
-  int server_fd = server_init(port);
-  log_message(LOG_INFO, "Server started. Waiting for clients...");
-
-  server_run(server_fd);
-
-  server_shutdown(server_fd);
-  log_message(LOG_INFO, "Server terminated.");
   return 0;
 }
